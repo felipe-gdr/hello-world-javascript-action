e6e8b6e7cc2d152ac7e2196de0b8674233c3a782
Felipe Reis <felipe.gdr@gmail.com>
2019-12-06 18:37:34 +1100

    Add simple graphql analyzis actions

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+
+console.log('This is a GraphQL Analyzis action');
+
+core.log('starting graphql-analyzis action');
+
+const configFilePath = path.join('.graphql-analyzer.json');
+
+core.log(`config file path: ${configFilePath}`);
+
+const configContent = fs.readFileSync(configFilePath, 'utf8');
+
+core.log(`config content: ${configContent}`);
+
+const config = JSON.parse(configContent);
+
+const schemaFilePath = path.join(config.schemaFile)
+
+core.log(`schema file path: ${schemaFilePath}`);
+
+const schema = fs.readFileSync(schemaFilePath, 'utf-8');
+
+core.log(`schema contents: ${schema}`)
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -9,7 +32,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
