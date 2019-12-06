3e12800f5850e645cae7b0c839206214777f0a5f
Felipe Reis <felipe.gdr@gmail.com>
2019-12-06 18:47:55 +1100

    Remove a couple of logs

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ core.log(`schema contents: ${schema}`)
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  console.log(`Hello::: ${nameToGreet}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
