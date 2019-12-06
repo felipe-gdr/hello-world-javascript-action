fb58ec479d6b03bc3a87aa5a8be1a437f8975d93
Felipe Reis <felipe.gdr@gmail.com>
2019-12-06 18:53:33 +1100

    Replace core.log with console.log

--- dist/index.js
+++ dist/index.js
@@ -244,25 +244,23 @@ const path = __webpack_require__(277);
 
 console.log('This is a GraphQL Analyzis action');
 
-core.log('starting graphql-analyzis action');
-
 const configFilePath = path.join('.graphql-analyzer.json');
 
-core.log(`config file path: ${configFilePath}`);
+console.log(`config file path: ${configFilePath}`);
 
 const configContent = fs.readFileSync(configFilePath, 'utf8');
 
-core.log(`config content: ${configContent}`);
+console.log(`config content: ${configContent}`);
 
 const config = JSON.parse(configContent);
 
 const schemaFilePath = path.join(config.schemaFile)
 
-core.log(`schema file path: ${schemaFilePath}`);
+console.log(`schema file path: ${schemaFilePath}`);
 
 const schema = fs.readFileSync(schemaFilePath, 'utf-8');
 
-core.log(`schema contents: ${schema}`)
+console.log(`schema contents: ${schema}`)
 
 try {
   // `who-to-greet` input defined in action metadata file
